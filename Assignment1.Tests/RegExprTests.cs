0716b558cbac6c70376edf6cb7c6fd8ca8cbf12d
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2021-09-11 15:23:53 +0200

    added test and implemented SplitLine() (#1)

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -4,5 +4,19 @@ namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void SplitLine_Given_Stream_Return_Stream_Of_Words()
+        {
+            //Assert
+            var input = new string[]{"foo", "Bar", " foo", ". foo is bar, BAR No 1 fOO"};
+
+            var expected = new string[]{"foo", "Bar", "foo", "foo", "is", "bar", "BAR", "No", "1", "fOO"};
+
+            //Act
+            var output = RegExpr.SplitLine(input);
+
+            //Assert
+            Assert.Equal(expected, output);
+        }
     }
 }
